c03e2f49a8879c6ee6e4bd57ff3f96cf1a93f899
Gmanlove <emmagmanc@gmail.com>
2023-09-04 22:34:02 +0100

    update

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -3,70 +3,88 @@ import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
-/**
- * Props declaration for <Graph />
- */
 interface IProps {
-  data: ServerRespond[],
+  data: ServerRespond[];
 }
 
-/**
- * Perspective library adds load to HTMLElement prototype.
- * This interface acts as a wrapper for Typescript compiler.
- */
-interface PerspectiveViewerElement {
-  load: (table: Table) => void,
+// Define the custom interface for PerspectiveViewerElement
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void;
+  setAttribute: (name: string, value: string) => void;
 }
 
-/**
- * React component that renders Perspective based on data
- * parsed from its parent through data property.
- */
 class Graph extends Component<IProps, {}> {
-  // Perspective table
   table: Table | undefined;
-
-  render() {
-    return React.createElement('perspective-viewer');
-  }
+  uniqueTimestamps: Set<number> = new Set(); // Maintain a set of unique timestamps
 
   componentDidMount() {
-    // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    // Define elem as PerspectiveViewerElement
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
       top_bid_price: 'float',
-      timestamp: 'date',
+      timestamp: 'datetime',
     };
 
-    if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
-    }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+    try {
+      // Ensure window.perspective and window.perspective.worker() are defined
+      if (window.perspective && window.perspective.worker) {
+        this.table = window.perspective.worker().table(schema);
+      } else {
+        throw new Error('Perspective or Perspective Worker is not available.');
+      }
 
-      // Add more Perspective configurations here.
-      elem.load(this.table);
+      if (this.table) {
+        elem.load(this.table);
+
+        // Set up the element's attributes as an object
+        elem.setAttribute('view', 'y_line');
+        elem.setAttribute('column-pivots', '["stock"]');
+        elem.setAttribute('row-pivots', '["timestamp"]');
+        elem.setAttribute('columns', '["top_ask_price"]');
+        elem.setAttribute(
+          'aggregates',
+          JSON.stringify({
+            stock: 'distinct count',
+            top_ask_price: 'avg',
+            top_bid_price: 'avg',
+            timestamp: 'distinct count',
+          })
+        );
+      }
+    } catch (error) {
+      console.error('Error in componentDidMount:', error);
     }
   }
 
   componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
-        return {
-          stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
-          timestamp: el.timestamp,
-        };
-      }));
+      const uniqueData = this.props.data.filter((el) => {
+        const timestamp = el.timestamp.getTime();
+        if (!this.uniqueTimestamps.has(timestamp)) {
+          this.uniqueTimestamps.add(timestamp);
+          return true;
+        }
+        return false;
+      });
+
+      this.table.update(
+        uniqueData.map((el: any) => {
+          return {
+            stock: el.stock,
+            top_ask_price: el.top_ask && el.top_ask.price || 0,
+            top_bid_price: el.top_bid && el.top_bid.price || 0,
+            timestamp: el.timestamp,
+          };
+        })
+      );
     }
+  }
+
+  render() {
+    return <div style={{ height: '100%' }} />;
   }
 }
 
